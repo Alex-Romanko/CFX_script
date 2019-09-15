d98c2e09c5c99d4e1325919549e0985980018129
saaha1995 <romanko.aleksandr.a@gmail.com>
2019-09-15 15:25:12 +0300

    started making

--- code/main.py
+++ code/main.py
@@ -1,2 +1,29 @@
+import os
+import shutil
+import csv
+
+print (os.getcwd())
+os.chdir ('../CFX_files/')
+print (os.getcwd())
+
+
+
+with open ('admin_2019-09-14 14-09-54_CFX96N6NEW.csv', 'r') as f:
+    print (f.name, f.mode)
+    for line in f:
+        print(line)
+
+
+
+
+try:
+    os.chdir ('../datatable')
+except FileNotFoundError:
+    os.chdir ('../')
+    os.mkdir ('datatable')
+os.chdir ('../datatable')
+
+
+
 if __name__ == "__main__":
     pass
