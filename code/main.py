f9f94bd04d3358feb346c2fa77c69206a6833421
saaha1995 <romanko.aleksandr.a@gmail.com>
2019-09-28 22:12:28 +0300

    fix archive

--- code/main.py
+++ code/main.py
@@ -4,14 +4,16 @@ import shutil
 import csv
 import operator
 
+
+if not os.path.exists('../CFX_file_archive'):
+    os.mkdir('../CFX_file_archive')
+
 #print (os.getcwd())
 os.chdir ('../CFX_files/')
 #print (os.getcwd())
 
-
 for CFX_file in os.listdir(os.getcwd()):
     #print (str(CFX_file))
-
 
     file_name = 'default'
     sortedlist = []
@@ -23,9 +25,6 @@ for CFX_file in os.listdir(os.getcwd()):
             if line == start_sequence:
                 sortedlist = sorted(csv_reader, key=operator.itemgetter(4, 0, 1), reverse=False)
 
-
-
-
     sample_dict = {}
     for elem in sortedlist:
         if elem[4] in sample_dict:
@@ -33,9 +32,8 @@ for CFX_file in os.listdir(os.getcwd()):
         else:
             sample_dict[elem[4]] = [elem[5]]
 
-
     shutil.move('./' + str(CFX_file), '../CFX_file_archive')
-    
+
 
     os.chdir ('../')
     #print (os.getcwd())
@@ -43,10 +41,6 @@ for CFX_file in os.listdir(os.getcwd()):
         for key in sample_dict.keys():
             f.write("%s,%s\n"%(key,sample_dict[key]))
     os.chdir ('./CFX_files')
-    
-
-
-    
 
 
     #print (file_name)
