cd668a24e1880d4636db8ee1035f4081725b088c
saaha1995 <romanko.aleksandr.a@gmail.com>
2019-09-28 20:23:30 +0300

    it works

--- code/main.py
+++ code/main.py
@@ -4,41 +4,51 @@ import shutil
 import csv
 import operator
 
-print (os.getcwd())
+#print (os.getcwd())
 os.chdir ('../CFX_files/')
-print (os.getcwd())
+#print (os.getcwd())
 
 
-file_name = 'default'
-sortedlist = []
-start_sequence =['Well', 'Fluor', 'Target', 'Content', 'Sample', 'Cq', 'Starting Quantity (SQ)']
-with open ('admin_2019-09-14 14-09-54_CFX96N6NEW.csv', 'r') as f:
-    file_name = str(f.name)
-    csv_reader = csv.reader(f, delimiter=',')
-    for  line in csv_reader:
-        if line == start_sequence:
-            sortedlist = sorted(csv_reader, key=operator.itemgetter(4, 0, 1), reverse=False)
+for CFX_file in os.listdir(os.getcwd()):
+    #print (str(CFX_file))
 
 
-
-
-sample_dict = {}
-for elem in sortedlist:
-    if elem[4] in sample_dict:
-        sample_dict[elem[4]].append(elem[5])
-    else:
-        sample_dict[elem[4]] = [elem[5]]
-
-print (sample_dict['T12345'])
+    file_name = 'default'
+    sortedlist = []
+    start_sequence =['Well', 'Fluor', 'Target', 'Content', 'Sample', 'Cq', 'Starting Quantity (SQ)']
+    with open (CFX_file, 'r') as f:
+        file_name = str(f.name)
+        csv_reader = csv.reader(f, delimiter=',')
+        for  line in csv_reader:
+            if line == start_sequence:
+                sortedlist = sorted(csv_reader, key=operator.itemgetter(4, 0, 1), reverse=False)
 
 
 
 
+    sample_dict = {}
+    for elem in sortedlist:
+        if elem[4] in sample_dict:
+            sample_dict[elem[4]].append(elem[5])
+        else:
+            sample_dict[elem[4]] = [elem[5]]
 
 
+    shutil.move('./' + str(CFX_file), '../CFX_file_archive')
+    
+
+    os.chdir ('../')
+    #print (os.getcwd())
+    with open('Data_Table_Diagn.csv', 'a+') as f:
+        for key in sample_dict.keys():
+            f.write("%s,%s\n"%(key,sample_dict[key]))
+    os.chdir ('./CFX_files')
+    
 
 
+    
 
-print (file_name)
+
+    #print (file_name)
 if __name__ == "__main__":
     pass
