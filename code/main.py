8072b5058d5c124003316728d37b548621b0f10a
saaha1995 <romanko.aleksandr.a@gmail.com>
2019-11-02 22:30:33 +0300

    some changes

--- code/main.py
+++ code/main.py
@@ -5,17 +5,19 @@ import operator
 from pathlib import Path
 # you have to run the file from "./code/" directory
 
-def CFX_file_reader (CFX_file):
+
+def CFX_file_reader(CFX_file):
     sortedlist = []
-    start_sequence =['Well', 'Fluor', 'Target', 'Content', 'Sample', 'Cq', 'Starting Quantity (SQ)']
-    with open (CFX_file, 'r') as f:
+    start_sequence = ['Well', 'Fluor', 'Target', 'Content', 'Sample', 'Cq', 'Starting Quantity (SQ)']
+    with open(CFX_file, 'r') as f:
         csv_reader = csv.reader(f, delimiter=',')
-        for  line in csv_reader:
+        for line in csv_reader:
             if line == start_sequence:
                 sortedlist = sorted(csv_reader, key=operator.itemgetter(4, 0, 1), reverse=False)
     return (sortedlist)
 
-def sample_dict_former (sortedlist):
+
+def sample_dict_former(sortedlist):
     sample_dict = {}
     for elem in sortedlist:
         if elem[4] in sample_dict:
@@ -24,7 +26,8 @@ def sample_dict_former (sortedlist):
             sample_dict[elem[4]] = [elem[5]]
     return (sample_dict)
 
-def formating_dict (sample_dict):
+
+def formating_dict(sample_dict):
     form_dict = {}
     for key in sample_dict:
         for value in sample_dict[key]:
@@ -32,45 +35,48 @@ def formating_dict (sample_dict):
             if value == 'NaN':
                 value = 38
             try:
-                value = round(float (value), 1)
+                value = round(float(value), 1)
                 if key in form_dict:
                     form_dict[key].append(value)
                 else:
                     form_dict[key] = [value]
-            except:
-                
+            except TypeError:
                 if key in form_dict:
                     form_dict[key].append(value)
                 else:
                     form_dict[key] = [value]
     return (form_dict)
 
-def data_writer (form_dict):
+
+def data_writer(form_dict):
     with open('Data_Table_Diagn.csv', 'a+') as f:
         f.write(str(CFX_file))
         f.write('\n')
         for key in form_dict.keys():
-            f.write("%s,%s\n"%(key,form_dict[key]))
+            f.write("%s,%s\n" % (key, form_dict[key]))
 
-def move_file_to_archive (CFX_file):
+
+def move_file_to_archive(CFX_file):
     destination = Path('../CFX_file_archive')
-    source = './' + str(CFX_file)   # i dont know how to do it for workin in windows and unixlike
+    source = './' + str(CFX_file)
     shutil.move(source, destination)
 
-def strar_script ():
+
+def strar_script():
     if not os.path.exists(Path('../CFX_file_archive')):
         os.mkdir(Path('../CFX_file_archive'))
-    os.chdir (Path('../CFX_files/'))
+    os.chdir(Path('../CFX_files/'))
+
 
 if __name__ == "__main__":
-    strar_script ()
+    strar_script()
     for CFX_file in os.listdir(os.getcwd()):
         if CFX_file in os.listdir(Path('../CFX_file_archive')):
             continue
-        sortedlist = CFX_file_reader (CFX_file)
-        sample_dict = sample_dict_former (sortedlist)
+        sortedlist = CFX_file_reader(CFX_file)
+        sample_dict = sample_dict_former(sortedlist)
         form_dict = formating_dict(sample_dict)
-        move_file_to_archive (CFX_file)
-        os.chdir (Path('../'))
-        data_writer (form_dict)
-        os.chdir (Path('./CFX_files'))
+        move_file_to_archive(CFX_file)
+        os.chdir(Path('../'))
+        data_writer(form_dict)
+        os.chdir(Path('./CFX_files'))
